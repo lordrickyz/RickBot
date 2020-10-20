d88e704a61af8c3ebb2eeae3fcc2c82f9da9ee35
lordrickyz <rickyzheng@ymail.com>
2020-10-19 22:46:06 -0400

    Add Resume in JSON format

---
+++ features/resume.js
@@ -0,0 +1,287 @@
+/* const resumeObj = require('resume') */
+
+export default resume = {
+  "$schema": "https://json.schemastore.org/resume",
+  "basics": {
+    "name": "Ricky Zheng",
+    "label": "Software Engineer  | Javascript | React.js | Redux.js | Ruby | Ruby on Rails | Python | Node.js | MongoDB",
+    "image": "",
+    "email": "rickyzheng@ymail.com",
+    "phone": "",
+    "url": "",
+    "summary": "I am life insurance agent turned software engineer who loves tackling complex problems and someone who always enjoys challenges, teamwork, and surpassing any goals set forth on me or my team. I have experiences in JavaScript, Ruby, Rails, React, Python3, and more. I am adaptable, open-minded, value all learning experiences and eager to expand on what I already know.",
+    "location": {
+      "address": "New York, New York",
+      "postalCode": "",
+      "city": "",
+      "countryCode": "US",
+      "region": ""
+    },
+    "profiles": [
+      {
+        "network": "LinkedIn",
+        "username": "rickyzhengs",
+        "url": "https://www.linkedin.com/in/rickyzhengs/"
+      }
+    ]
+  },
+  "work": [
+    {
+      "name": "Personal Projects",
+      "endDate": "",
+      "highlights": [],
+      "position": "Software Engineer",
+      "startDate": "2020-03-31",
+      "summary": "GroupRide | MongoDB , Express.js , React , Node.js , Webpack , GIMP |        \t            \nMERN stack application that allows users to schedule and create biking events with safest routes:\n• Utilized React and Redux to dynamically present information according to current state allowing the user to login through modals and interact with event feeds.\n• Lead frontend designs, providing creative and efficient inputs and enabling RESTful calls to be serviced for UI applications supporting data fetching from the client.\n• Incorporated Express.Js to retrieve promises connecting stored data in MongoDB and using React to manage the current state allowing users to retrieve their session to either log in or sign up.\n\nJello | JavaScript , React / Redux , Ruby on Rails(RoR) , PostgreSQL , CSS3,  Webpack |        \nReact and Ruby on Rails Kanban tool; collaboration tool that organizes your projects into boards.\n• Utilized jbuilders to establish connections between Rails and React by querying JSON using AJAX requests to allow middlewares to receive RESTful information to be sent to frontend and processed for rendering.\n•  Integrated React Drag and Drop library to React UI to allow swapping of content from list to another list.\n•  Leveraged BCrypt Hashing to secure user passwords from backend to frontend. Preventing malicious users from accessing actual passwords in the frontend. Session is stored in cookies, but nulls when a user logs out.\n\nBubble Pop | JavaScript, CSS3 , HTML5 , Heroku , Webpack |\nInteractive JavaScript game where users shoot bubbles to match other bubbles until the board is cleared:\n•  Implemented hexagonal collision algorithms in pure JavaScript to allow bubbles to snap onto a relative position on the board after colliding with either the top of the board or another bubble object.\n•  Incorporated HTML5 canvas for UI design of the game and rendition of game mechanics such as the bubble, the launcher, and the board.",
+      "url": ""
+    },
+    {
+      "name": "World Financial Group (WFG)",
+      "endDate": "2020-03-31",
+      "highlights": [],
+      "position": "Licensed Insurance Agent",
+      "startDate": "2019-04-30",
+      "summary": "• Built and maintain a client base; keep clients informed about products and services for, but not limited to, life insurance, health insurance, retirement plans and annuities.\n• Built and led training sessions for new onboarding members; teaching best practice of Salesforce and financial training.\n•  Aid the underwriter in assessing risk by evaluating all factors using detailed information from prospective to help minimize loss",
+      "url": ""
+    },
+    {
+      "name": "Railcare Health",
+      "endDate": "2019-05-31",
+      "highlights": [],
+      "position": "Communications Manager",
+      "startDate": "2017-01-31",
+      "summary": "• Assisting members in establishing organizational marketing, branding, and communication plans. Establishing network allowing efficient referrals and volunteer sourcing.\n• Secured proper referrals and documentations for any authorized administration.\n• Ensured activities are under proper protocol and oversee the teamwork and collaboration within a committee.",
+      "url": ""
+    }
+  ],
+  "volunteer": [
+    {
+      "organization": "UNC Health Care",
+      "position": "Health Volunteer",
+      "url": "https://www.linkedin.com/company/166587",
+      "startDate": "2017-05-31",
+      "endDate": "2018-07-31",
+      "summary": "Volunteer within the Geriatric Department. \n• Providing social services to geriatric patients and assist staff or nurses with visitor communications.",
+      "highlights": []
+    },
+    {
+      "organization": "Southeastern Regional Medical Center",
+      "position": "Junior Assistant",
+      "url": "",
+      "startDate": "2012-06-30",
+      "endDate": "2016-08-31",
+      "summary": "Volunteered for Southeastern Regional Medical Center Pediatrics Floor for two years. \n• Utilized communications and patient interacting skills to provide patients (underage/of age) with moral support when a family member does not show up. \n• Demonstrated the ability to communicate with patients that are in need of support and coping situations.\n\nVolunteered at the Gibson Cancer Center from June through August for three years. \n• Utilized communications and patient interacting skills to provide patients with support and understanding. \n• Demonstrated organization by filing and organizing documents of patients under the authorization of the nurse or the employed personnel",
+      "highlights": []
+    }
+  ],
+  "education": [
+    {
+      "institution": "App Academy",
+      "area": "Computer Software Engineering",
+      "studyType": "",
+      "startDate": "2020-12-31",
+      "endDate": "",
+      "gpa": "",
+      "courses": []
+    },
+    {
+      "institution": "University of North Carolina at Chapel Hill",
+      "area": "Biological Science, Chemistry, Interdisciplinary Asian Studies",
+      "studyType": "Bachelor of Science - BS",
+      "startDate": "2015-12-31",
+      "endDate": "2019-12-31",
+      "gpa": "",
+      "courses": []
+    }
+  ],
+  "awards": [],
+  "publications": [],
+  "skills": [
+    {
+      "name": "JavaScript",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "React.js",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Ruby on Rails",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Front-end Development",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Back-End Web Development",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Search Engine Optimization (SEO)",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Webpack",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "MongoDB",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Chinese",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Zoom",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Amazon Web Services (AWS)",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Github",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Microsoft Word",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Web Development",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Google Maps API",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Cascading Style Sheets (CSS)",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Microsoft Office",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Node.js",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "ruby",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "GIMP",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Microsoft PowerPoint",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "SQL",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Full-Stack Development",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "AJAX",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Microsoft Excel",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Express.js",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "HTML",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Redux",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Git",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Ruby",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "PostgreSQL",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Adobe Photoshop",
+      "level": "",
+      "keywords": []
+    }
+  ],
+  "languages": [
+    {
+      "language": "en",
+      "fluency": "Native Speaker"
+    }
+  ],
+  "interests": [],
+  "references": [
+    {
+      "name": "Colton Wilmouth",
+      "reference": "I can honestly say that anyone would enjoy working with Ricky. He is always aware of others and apt to hear their opinions. He has no problem jumping right in and enjoys testing procedures for their efficiency. we built some pretty cool things together at App Academy. If you want someone to add perspective to your team, look here first!"
+    },
+    {
+      "name": "Isaac Owens",
+      "reference": "Ricky is a fantastic person to work with.  Ricky and I worked on a particularly difficult project together while we were students together at App Academy.  He was always encouraging and willing to try new things when problems seemed impossible.  I really enjoyed Ricky's ability to ask the best questions at the right times to get the creative juices flowing.  He is humble, passionate, and eager to soak up more and more every day.  I know that wherever Ricky ends up applying his talents he will work hard and succeed!"
+    },
+    {
+      "name": "Elvin Valette",
+      "reference": "“Working with Ricky was a truly great experience. He is a brilliant well rounded software engineer with experience in both frontend and backend development. Ricky always had unique out of the box solutions when debugging. Honestly a jack of all trades and all around great person."
+    },
+    {
+      "name": "Eric Smith",
+      "reference": "Ricky deftly weaves kindness and drive into everything he works on. Through our numerous experiences pairing and discussing personal projects outside of class, he was always there to share, guide and be a friend and partner.  His passion for programming meant he was always willing to go above and beyond what was asked, and his follow through meant his ambitions always came to fruition - whether those ambitions lay in incorporating new libraries or working throughout the stack. Working with Ricky was always a treat, and anybody would be lucky to have him as a part of their team."
+    }
+  ],
+  "projects": []
+}
